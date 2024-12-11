8fc46006e835b8f41fa86761008714488bdb3c18
Nixbat <nixpat08@gmail.com>
2024-12-11 09:17:58 -0500

    first commit

---
+++ autocomplete.js
@@ -0,0 +1,42 @@
+let availableKeywords = [
+    "HTML",
+    "CSS",
+    "Tutorials",
+    "Web design",
+    "JavaScript",
+    "Where to learn coding online",
+    "Where to learn web design",
+    "How to create a website",
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function() {
+    let result = [];
+    let input = inputBox.value;
+
+    if(input.length) {
+        result = availableKeywords.filter((keyword) => {
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });              
+    }
+    display(result);
+
+    if(!result.length) {
+        resultsBox.innerHTML = "";
+    }
+}
+
+function display(result) {
+    const content = result.map((list) =>{
+        return "<li onclick=selectInput(this)>" +list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join("") + "</ul>";
+}
+
+function selectInput(list) {
+    inputBox.value = list.innerHTML;
+    resultsBox.innerHTML = "";
+}
